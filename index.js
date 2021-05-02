b45bdcde7c3ac172e9b1e7eb1bdd09cf74710838
elcioroberto95 <elcioroberto1995@gmail.com>
2021-05-02 15:37:41 -0300

    feat: implement class project tile

--- index.js
+++ index.js
@@ -23,8 +23,55 @@ class Player {
         pincel.fill();
     }
 
-}
-const x = canvas.width/2;
-const y = canvas.height/2
-const player = new Player(x,y,30,'blue');
+};
+
+class ProjectTile {
+    constructor(x,y,radius,color,velocity){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    };
+    draw(){
+        pincel.beginPath();
+        pincel.arc(this.x,this.y,this.radius,0,Math.PI * 2,false);
+        pincel.fillStyle = this.color;
+        pincel.fill();
+    };
+    update(){
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    };
+
+};
+
+
+const widthPlayer = canvas.width/2;
+const heightPlayer = canvas.height/2
+const player = new Player(widthPlayer,heightPlayer,30,'blue');
 player.draw();
+
+const projectTile = new ProjectTile(
+    canvas.width/2,
+    canvas.height/2,
+    5,
+    'red',
+    {
+    x: 1,
+    y: 1
+   }
+   );
+  
+
+function animate(){
+    requestAnimationFrame(animate);
+    projectTile.draw();
+    projectTile.update();
+}
+
+window.addEventListener('click', ()=> {
+  
+})
+
+animate();
